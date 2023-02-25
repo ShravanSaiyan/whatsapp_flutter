459639104d44cf64a35a0e092f13f96ba37122e0
ShravanSaiyan <shravankumar.sk50@gmail.com>
2023-02-25 12:53:23 +0530

    Added Firebase Configurations

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:whatsapp_flutter/colors.dart';
+import 'package:whatsapp_flutter/firebase_options.dart';
 import 'package:whatsapp_flutter/layouts/responsive_layout.dart';
 import 'package:whatsapp_flutter/screens/mobile_screen_layout.dart';
 import 'package:whatsapp_flutter/screens/web_screen_layout.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
